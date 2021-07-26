cf9d18f490f3fe576a29f9f81a9067dea5c47347
Your Name <xxxhxh622@163.com>
2021-07-26 17:31:55 +0800

    first commit

---
+++ helloworld.lua
@@ -0,0 +1,2 @@
+-- http://www.dba.cn/book/lua/LUAJiaoCheng/LUAJiBenYuFa.html
+print("Hello World!");
---
+++ 注释.lua
@@ -0,0 +1,7 @@
+-- Lua 5.2
+-- print("Lua 5.2") 单行注释
+print("Lua 5.2")
+--[[
+    多行注释
+]]
+print("Hello World")
